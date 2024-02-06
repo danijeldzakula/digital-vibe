0dc159814f31f35a2f0437c8d9fc844b6de9b3d8
Danijel Džakula <danijeldzakula1991@gmail.com>
2024-02-07 00:56:39 +0100

    undo start project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,11 @@ import { StoreProvider } from '@/utils/redux/StoreProvider';
 import { Main } from '@/content/Content';
 import { Inter } from 'next/font/google';
 import '@/styles/globals.css';
-import Header from '@/components/header';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  manifest: '/manifest.json',
-  title: {
-    default: 'My Portfolio',
-    template: '%s | My Portfolio'
-  }
+  manifest: '/manifest.json'
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -20,8 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className}>
         <StoreProvider>
-          <Header />
-          <Main className='mt-8'>{children}</Main>
+          <Main>{children}</Main>
         </StoreProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import { Container } from "@/content/Content";
 
 export const metadata = {
-  title: "Discover the Artistry of Danijel Dzakula | Web Vibe",
-  description: "Certainly! Here's a description for your portfolio home page Welcome to my portfolio! As a passionate front-end developer, I invite you to explore a showcase of my programming projects and creations. From captivating web designs to innovative user interfaces, each project reflects my dedication to crafting engaging digital experiences. Join me on this journey through code and design as we delve into the world of front-end development together. Get inspired, discover my skills, and witness the artistry behind each project. Let's connect and collaborate to bring your ideas to life! Welcome to a world where creativity meets functionality.",
+  title: "Demo",
+  description: "Demo",
 };
 
 export default async function Home() {
   return (
     <Container>
-      <h1>Home Page</h1>
+      <h1>Home</h1>
     </Container>
   )
 }
